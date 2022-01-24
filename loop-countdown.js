1b0ca3168e937e693d9b225165c417d78e74ec36
ElisaMamolo <49635343+ElisaMamolo@users.noreply.github.com>
2022-01-24 12:49:46 +0100

    Update loop-countdown.js

--- loop-countdown.js
+++ loop-countdown.js
@@ -10,3 +10,19 @@ if (n < 1) {
 
 
 ("https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-use-recursion-to-create-a-countdown/305925/2")
+
+
+//range of numbers
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum - startNum === 0) {
+    return [startNum];
+  } else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+};
+
+//rangeOfNumbers(1, 5) should return [1, 2, 3, 4, 5].
+//rangeOfNumbers(6, 9) should return [6, 7, 8, 9].
+//rangeOfNumbers(4, 4) should return [4]
